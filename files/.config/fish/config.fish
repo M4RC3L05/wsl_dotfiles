b4bf20b06bccc85c8ad055a2995798d32e1a99c1
m4rc3l05 <joaobat@live.com.pt>
2021-03-07 14:08:48 +0000

    Config wsl to gwsl
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -27,15 +27,35 @@ set -g fish_color_user brgreen
 set -g fish_color_valid_path --underline
 
 # code 
-set -x PATH "/mnt/c/Users/joaob/AppData/Local/Programs/Microsoft VS Code Insiders/bin" $PATH 
+set -x PATH "/mnt/c/Users/joaob/AppData/Local/Programs/Microsoft VS Code Insiders/bin" $PATH
 
 # docker
-set -x PATH "/mnt/c/Program Files/Docker/Docker/resources/bin" $PATH 
-set -x PATH /mnt/c/ProgramData/DockerDesktop/version-bin $PATH 
+set -x PATH "/mnt/c/Program Files/Docker/Docker/resources/bin" $PATH
+set -x PATH /mnt/c/ProgramData/DockerDesktop/version-bin $PATH
 
 if not type -q node
-    nvm use lts > /dev/null
+    nvm use lts >/dev/null
 end
 
 # starship init fish | source
 oh-my-posh --init --shell fish --config ~/.poshthemes/star-custom.omp.json | source
+
+# GWSL
+set --export WSL2 1
+set ipconfig_exec (wslpath "C:\\Windows\\System32\\ipconfig.exe")
+if which ipconfig.exe >/dev/null
+    set ipconfig_exec (which ipconfig.exe)
+end
+
+set wsl2_d_tmp (eval $ipconfig_exec | grep -n -m 1 "Default Gateway.*: [0-9a-z]" | cut -d : -f 1)
+if test -n "$wsl2_d_tmp"
+    set first_line (expr $wsl2_d_tmp - 4)
+    set wsl2_d_tmp (eval $ipconfig_exec | sed $first_line,$wsl2_d_tmp!d | grep IPv4 | cut -d : -f 2 | sed -e "s|\s||g" -e "s|\r||g")
+    set --export DISPLAY "$wsl2_d_tmp:0"
+    set -e first_line
+else
+    set --export DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+end
+
+set -e wsl2_d_tmp
+set -e ipconfig_exec
