6b5c45123a832bc517fb9b1ee0b2c3b5d897324b
m4rc3l05 <joaobat@live.com.pt>
2021-07-04 02:06:02 +0100

    Add comments to _placeFiles in install script
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

--- install.fish
+++ install.fish
@@ -5,26 +5,35 @@ set -l realFishDir "~/.config/fish"
 set -l files "$rootDir/files"
 
 function _placeFiles
+    # Sets source root dir
     set -l root $argv[1]
+    # Sets destination dir
     set -l to $argv[2]
 
     for file in $root/.* $root/*
+        # If source file if a directory call _placeFiles recursivly with new source, and destination root
         if test -d $file
             set -l bn (basename $file)
             _placeFiles "$root/$bn" "$to/$bn"
         else
+            # Path of potentialy already existting file
             set -l replaced (string replace -a "$root" "$to" "$file")
+            # Gets folder name of containing existing file
             set -l dn (dirname $replaced)
+            # Gets file name of already existsing file
             set -l bn (basename $replaced)
 
+            # If the folder name do not exists, create it
             if not test -d $dn
                 mkdir -p $dn
             end
 
+            # If potentialy already existting file exists remove it in order to update with new version
             if test -e $replaced
                 rm -rf $replaced
             end
 
+            # Symlink local file with path of potentialy already existting file
             ln -s $file $replaced
         end
     end
